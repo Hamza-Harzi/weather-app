8ac4de91e189e758003f306d742be3fb546b7109
Hamza-Harzi <97437135+Hamza-Harzi@users.noreply.github.com>
2023-04-15 03:23:27 +0200

    weather app with nuxt

---
+++ app.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+const search = ref("Toronto");
+const input = ref("");
+const { data: city, error } = useFetch(
+  () =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=957ae1209925fcd33d53ee8c813ab383`
+);
+
+const handleClick = () => {
+  const formatedSearch = input.value.trim().split(" ").join("+");
+
+  search.value = formatedSearch;
+  input.value = "";
+};
+</script>
+
+<template>
+  <div class="h-screen relative overflow-hidden">
+    <img />
+    <div class="absolute w-full h-full top-0 overlay" />
+    <div class="absolute w-full h-full top-0 p-48">
+      <div class="flex justify-between">
+        <div>
+          <h1 class="text-7xl text-white">{{ city.name }}</h1>
+          <p class="font-extralight text-2xl mt-2 text-white">Sunday dec 9th</p>
+          <img
+            :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@4x.png`"
+            class="w-56 icon"
+          />
+        </div>
+        <div>
+          <p class="text-9xl text-white font-extralight">
+            {{ city.main.temp }}
+          </p>
+        </div>
+      </div>
+      <div class="mt-20">
+        <input
+          type="text"
+          class="w-1/2 h-10"
+          placeholder="Search a city..."
+          v-model="input"
+        />
+        <button class="bg-sky-400 w-20 text-white h-10" @click="handleClick">
+          Search
+        </button>
+      </div>
+    </div>
+  </div>
+  <!-- <div class="p-10">
+    <h1 class="text-7xl">Oops, we can't find that city</h1>
+    <button class="mt-5 bg-sky-400 px-10 w-50 text-white h-10">Go Back</button>
+  </div> -->
+</template>
+
+<style scoped>
+.overlay {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.icon {
+  margin-left: -2.75rem;
+  margin-top: -2.75rem;
+}
+</style>
