767d198155c0af08b7859af5b733f01cbabbe73b
Hamza-Harzi <97437135+Hamza-Harzi@users.noreply.github.com>
2023-04-15 22:50:05 +0200

    Error Handling

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-const search = ref("Toronto");
+const cookie = useCookie("city");
+
+if (!cookie.value) cookie.value = "Toronto";
+const search = ref(cookie.value);
 const input = ref("");
 const background = ref("");
 // const { data: city, error } = useFetch(
@@ -10,25 +13,35 @@ const background = ref("");
 const { data: city, error } = useAsyncData(
   "city",
   async () => {
-    const response = await $fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=957ae1209925fcd33d53ee8c813ab383`
-    );
+    let response;
+    try {
+      response = await $fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search.value}`,
+        {
+          params: {
+            units: "metric",
+            appid: process.env.WEATHER_APP_SECRET,
+          },
+        }
+      );
 
-    const temp = response.main.temp;
+      cookie.value = search.value;
 
-    if (temp <= -10) {
-      background.value =
-        "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
-    } else if (temp > -10 && temp <= 0) {
-      background.value =
-        "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
-    } else if (temp > 0 && temp <= 10) {
-      background.value =
-        "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
-    } else {
-      background.value =
-        "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
-    }
+      const temp = response.main.temp;
+      if (temp <= -10) {
+        background.value =
+          "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > -10 && temp <= 0) {
+        background.value =
+          "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > 0 && temp <= 10) {
+        background.value =
+          "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
+      } else {
+        background.value =
+          "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+      }
+    } catch (e) {}
     return response;
   },
   {
@@ -36,23 +49,34 @@ const { data: city, error } = useAsyncData(
   }
 );
 
+let today = new Date();
+today = today.toLocaleDateString("en-US", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+});
+
 const handleClick = () => {
   const formatedSearch = input.value.trim().split(" ").join("+");
-
   search.value = formatedSearch;
   input.value = "";
+};
+
+const goBack = () => {
+  search.value = cookie.value;
 };
 </script>
 
 <template>
-  <div class="h-screen relative overflow-hidden">
+  <div v-if="city" class="h-screen relative overflow-hidden">
     <img :src="background" />
     <div class="absolute w-full h-full top-0 overlay" />
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
         <div>
           <h1 class="text-7xl text-white">{{ city.name }}</h1>
-          <p class="font-extralight text-2xl mt-2 text-white">Sunday dec 9th</p>
+          <p class="font-extralight text-2xl mt-2 text-white">{{ today }}</p>
           <img
             :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@4x.png`"
             class="w-56 icon"
@@ -77,10 +101,12 @@ const handleClick = () => {
       </div>
     </div>
   </div>
-  <!-- <div class="p-10">
+  <div v-else class="p-10">
     <h1 class="text-7xl">Oops, we can't find that city</h1>
-    <button class="mt-5 bg-sky-400 px-10 w-50 text-white h-10">Go Back</button>
-  </div> -->
+    <button class="mt-5 bg-sky-400 px-10 w-50 text-white h-10" @click="goBack">
+      Go Back
+    </button>
+  </div>
 </template>
 
 <style scoped>
